bbd91012a9912732fec112460d6e617e13eeb810
WellyX4869 <wellysusanto4869@gmail.com>
2020-04-23 18:43:16 +0700

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DetailComponent } from './detail/detail.component';
+import { Routes, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
+const ROUTES: Routes =[
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'detail', component: DetailComponent},
+  {path: 'detail/:id', component: DetailComponent}
+]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(ROUTES)],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, LoginComponent, DetailComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private router: Router){}
+
+  LOGIN(){
+    this.router.navigate(['/login']);
+  }
+}
